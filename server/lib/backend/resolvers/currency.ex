f66ee744fd0218bb33a8e53591da418b37d27c38
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-04-16 08:28:13 +1000

    Delete server code, mv client code

--- server/lib/backend/resolvers/currency.ex
+++
@@ -1,6 +0,0 @@
-defmodule Backend.Resolvers.Currency do
-  def list_currencies(_parent, _args, _resolution) do
-    IO.puts "here..."
-    {:ok, [%{id: "bitcoin", name: "Bitcoin", symbol: "BTC"}]}
-  end
-end
--- server/lib/backend/schemas/currency.ex
+++
@@ -1,9 +0,0 @@
-defmodule Backend.Schema.Currency do
-  use Absinthe.Schema.Notation
-
-  object :currency do
-    field(:name, :string)
-    field(:id, :id)
-    field(:symbol, :string)
-  end
-end
